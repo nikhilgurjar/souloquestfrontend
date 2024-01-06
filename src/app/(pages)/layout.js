2aa90b05df2b722d6809fca7950b37abef8e971f
deepanshu agarwal <deepanshuagarwal9999@gmail.com>
2024-01-06 13:46:47 +0530

    remove file pages

--- src/app/(pages)/layout.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import MainLayout from "../layouts/MainLayout";
-
-const layout = ({ children }) => {
-  return <MainLayout>{children}</MainLayout>;
-};
-
-export default layout;
