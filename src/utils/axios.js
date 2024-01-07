d5b974bbc9415b41bd22c2c7c8d655ac983ac93e
nikhilgurjar <gurjarnikhil2022@gmail.com>
2024-01-07 18:33:27 +0530

    added server url

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 // import { HOST_API_KEY } from '../config-global';
 const HOST_API_KEY = '';
 // ----------------------------------------------------------------------
-const axiosInstance = axios.create({ baseURL: 'http://localhost:4000', withCredentials: true });
+const axiosInstance = axios.create({ baseURL: 'https://souloquest-backend.onrender.com', withCredentials: true });
 axiosInstance.interceptors.response.use((response) => response, (error) => {
     console.log(error);
     if(error.response?.status === 403){
